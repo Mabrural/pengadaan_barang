86e69b89c504faa2640807605b154baca8eaa97c
Mabrural <mabruralmutaqi@gmail.com>
2024-01-06 13:58:44 +0700

    halaman admin sudah oke dan bisa approve, tp belum ada pembeda antara hak akses admin dengan user biasa

---
+++ production/page/admin_approve/admin_aprove.php
@@ -0,0 +1,121 @@
+
+<?php
+
+$id_user = $_SESSION["id_user"];
+
+// $pengajuan = query("SELECT * FROM barang WHERE barang.id_barang=$id_user");
+
+?>
+    <div class="x_panel">
+      <div class="x_title">
+        <h2>Table Pengajuan Barang <small></small></h2>
+        <!-- <a href="?form=tambahPengajuan" class="btn btn-primary">Form Pengajuan</a> -->
+        <!-- <ul class="nav navbar-right panel_toolbox">
+          <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Settings 1</a>
+                <a class="dropdown-item" href="#">Settings 2</a>
+              </div>
+          </li>
+          <li><a class="close-link"><i class="fa fa-close"></i></a>
+          </li>
+        </ul> -->
+        <div class="clearfix"></div>
+      </div>
+
+      <div class="x_content">
+
+        <!-- <p>Add class <code>bulk_action</code> to table for bulk actions options on row select</p> -->
+
+        <div class="table-responsive">
+          <table class="table table-striped jambo_table bulk_action">
+            <thead>
+              <tr class="headings">
+                <!-- <th>
+                  <input type="checkbox" id="check-all" class="flat">
+                </th> -->
+                <th class="column-title">No. </th>
+                <th class="column-title">INV ID </th>
+                <th class="column-title">Nama Barang </th>
+                <th class="column-title">Spec </th>
+                <th class="column-title">Desc </th>
+                <th class="column-title">Qty </th>
+                <th class="column-title">Date in </th>
+                <th class="column-title">Unit Price </th>
+                <th class="column-title">Vendor </th>
+                <th class="column-title">Waranty </th>
+                <th class="column-title">Renewal </th>
+                <th class="column-title">Condition </th>
+                <th class="column-title">Nama Pemohon </th>
+                <th class="column-title">Status </th>
+                <th class="column-title no-link last"><span class="nobr">Action</span>
+                </th>
+                <th class="bulk-actions" colspan="7">
+                  <a class="antoo" style="color:#fff; font-weight:500;">Bulk Actions ( <span class="action-cnt"> </span> ) <i class="fa fa-chevron-down"></i></a>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr class="even pointer">
+              	<?php 
+              		$no = 1;
+              		$query = "SELECT * FROM barang JOIN user ON user.id_user=barang.id_user";
+              		// $query = "SELECT * FROM barang WHERE barang.id_user=$id_user";
+              		$tampil = mysqli_query($koneksi, $query);
+              		while ($data = mysqli_fetch_assoc($tampil)) {
+              		
+              		
+
+              	 ?>
+                <td class=" "><?= $no++;?></td>
+                <td class=" "><?= $data['inv_id'];?></td>
+                <td class=" "><?= $data['nama_barang'];?> </td>
+                <td class=" "><?= $data['spek'];?></td>
+                <td class=" "><?= $data['deskripsi'];?></td>
+                <td class=" "><?= $data['qty'];?></td>
+                <td class=" "><?= $data['tgl_masuk'];?></td>
+                <td class=" "><?= $data['harga'];?></td>
+                <td class=" "><?= $data['vendor'];?></td>
+                <td class=" "><?= $data['waranty'];?></td>
+                <td class=" "><?= $data['renewal'];?></td>
+                <td class=" "><?= $data['kondisi'];?></td>
+                <td class=" "><strong><?= $data['username'];?></strong></td>
+                <td class=" " style="color: <?php
+                    if ($data['status'] == 'Menunggu Persetujuan') {
+                        echo '#b58709';
+                    } elseif ($data['status'] == 'Sedang diproses') {
+                        echo '#2657a6';
+                    } elseif ($data['status'] == 'Sudah disetujui') {
+                        echo '#14a664';
+                    } else {
+                        echo '#a62f26';
+                    }
+                ?>;">
+                    <strong><?= $data['status'];?></strong>
+
+                 <?php if ($data['status'] == 'Sudah disetujui') { ?>
+		          <td class=" last">
+		            <span class="text-success fa fa-check"><strong> Selesai</strong></span>
+		          </td>
+		        <?php } else { ?>
+		          <td class=" last"><a href="?form=ubahApprove&id_barang=<?= $data["id_barang"]; ?>" class="btn btn-info btn-sm">Approve </a> 
+		        </td>
+		        <?php } ?>
+
+             <!--    <td class=" last"><a href="?form=ubahPengajuan&id_barang=<?= $data["id_barang"]; ?>" class="btn btn-info btn-sm">Approve </a> 
+                </td> -->
+              </tr>
+              
+           
+            </tbody>
+           <?php } ?>
+          </table>
+        </div>
+				
+			
+      </div>
+    </div>
---
+++ production/page/admin_approve/konfirmasi_aprove.php
@@ -0,0 +1,214 @@
+<?php
+
+// $id_mhs = $_SESSION["id_mhs"];
+
+// ambil data di URL
+$id_barang = $_GET["id_barang"];
+// query data mahasiswa berdasarkan id
+$barang = query("SELECT * FROM barang WHERE id_barang = $id_barang")[0];
+
+
+
+// cek apakah tombol submit sudah ditekan atau belum
+if (isset($_POST["submit"])) {
+	
+
+	
+
+	// cek apakah data berhasil diubah atau tidak
+	if(ubahPengajuan($_POST) > 0 ) {
+		echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+		echo '<script src="./sweetalert2.min.js"></script>';
+		echo "<script>
+		setTimeout(function () { 
+			swal.fire({
+				
+				title               : 'Berhasil',
+				text                :  'Data berhasil diapprove',
+				//footer              :  '',
+				icon                : 'success',
+				timer               : 2000,
+				showConfirmButton   : true
+			});  
+		},10);   setTimeout(function () {
+			window.location.href = '?page=pengajuan'; //will redirect to your blog page (an ex: blog.html)
+		}, 2000); //will call the function after 2 secs
+		</script>";
+		// echo "
+		// 	<script>
+		// 		alert('Data berhasil diubah!');
+		// 		document.location.href = '?page=pengajuan';
+		// 	</script>
+		// ";
+	} else{
+		echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+		echo '<script src="./sweetalert2.min.js"></script>';
+		echo "<script>
+		setTimeout(function () { 
+			swal.fire({
+				
+				title               : 'Gagal',
+				text                :  'Data gagal diubah',
+				//footer              :  '',
+				icon                : 'error',
+				timer               : 2000,
+				showConfirmButton   : true
+			});  
+		},10);   setTimeout(function () {
+			window.location.href = '?page=pengajuan'; //will redirect to your blog page (an ex: blog.html)
+		}, 2000); //will call the function after 2 secs
+		</script>";
+		// echo "
+		// 	<script>
+		// 		alert('Data gagal diubah!');
+		// 		document.location.href = '?page=pengajuan';
+		// 	</script>
+		// ";
+	}
+
+}
+
+
+?>
+
+
+    <div class="x_panel">
+      <div class="">
+					<div class="page-title">
+						<div class="title_left">
+							<h3>Konfirmasi Approval</h3>
+						</div>
+
+						<div class="title_right">
+							<div class="col-md-5 col-sm-5  form-group pull-right top_search">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Search for...">
+									<span class="input-group-btn">
+										<button class="btn btn-default" type="button">Go!</button>
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+					<div class="row">
+						<div class="col-md-12 col-sm-12 ">
+							<div class="x_panel">
+								<div class="x_title">
+									<h2>Konfirmasi Data <small></small></h2>
+									<!-- <ul class="nav navbar-right panel_toolbox">
+										<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+										</li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+											<ul class="dropdown-menu" role="menu">
+												<li><a class="dropdown-item" href="#">Settings 1</a>
+												</li>
+												<li><a class="dropdown-item" href="#">Settings 2</a>
+												</li>
+											</ul>
+										</li>
+										<li><a class="close-link"><i class="fa fa-close"></i></a>
+										</li>
+									</ul> -->
+									<div class="clearfix"></div>
+								</div>
+								<div class="x_content">
+									<br />
+									<form action="" method="post" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+										<input type="hidden" name="id_barang" value="<?= $barang["id_barang"];?>">
+										<div class="item form-group">
+											<!-- <input type="hidden" name="aksi" value="ubah"> -->
+
+											<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">INV ID <span class="required">*</span>
+											</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input type="text" name="inv_id" id="first-name" required="required" class="form-control" value="<?= $barang["inv_id"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Nama Barang <span class="required">*</span>
+											</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input type="text" name="nama_barang" id="last-name" required="required" class="form-control" value="<?= $barang["nama_barang"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Spec</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="spek" class="form-control" type="text" value="<?= $barang["spek"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Desc</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="deskripsi" class="form-control" type="text" value="<?= $barang["deskripsi"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Qty</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="qty" class="form-control" type="text" value="<?= $barang["qty"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Date In</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="tgl_masuk" class="form-control" type="date" value="<?= $barang["tgl_masuk"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Unit Price</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="harga" class="form-control" type="text" value="<?= $barang["harga"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Vendor</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="vendor" class="form-control" type="text" value="<?= $barang["vendor"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Waranty</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="waranty" class="form-control" type="text" value="<?= $barang["waranty"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Renewal</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="renewal" class="form-control" type="text" value="<?= $barang["renewal"];?>" readonly>
+											</div>
+										</div>
+										<div class="item form-group">
+											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Condition</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input id="middle-name" name="kondisi" class="form-control" type="text" value="<?= $barang["kondisi"];?>" readonly>
+												<input id="middle-name" name="status" class="form-control" type="hidden" value="Sudah disetujui">
+											</div>
+										</div>
+									
+										<div class="ln_solid"></div>
+										<div class="item form-group">
+											<div class="col-md-6 col-sm-6 offset-md-3">
+												<!-- <button class="btn btn-primary" type="button">Cancel</button> -->
+												<!-- <button class="btn btn-primary" type="reset">Reset</button> -->
+												<button type="submit" class="btn btn-success" name="submit">Approve</button>
+											</div>
+										</div>
+
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				
+
+
+
+
+					
+				</div>
+    </div>
