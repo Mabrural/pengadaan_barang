1849ab0051135779ec993b7c12bd5ad8e045a949
Mabrural <mabruralmutaqi@gmail.com>
2024-01-10 18:07:37 +0700

    update database sistem.sql, mengupdate nama session dari username menjadi nama_emp, agar yang ditampilkan di website adalah nama lengkapnya yang sudah terintegrasi dengan tabel user

--- sistem.sql
+++ sistem.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 09, 2024 at 11:44 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 10, 2024 at 12:05 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -46,10 +46,11 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `kode_pengajuan`, `nama_barang`, `spek`, `deskripsi`, `qty`, `tgl_pengajuan`, `status`, `acc1`, `acc2`, `id_user`) VALUES
-(110, 110989191219281927, 'Ranjang Tingkat', 'lampu 2 TL', 'Centronic', 1, '2024-01-09', 'Sudah disetujui', 'admin', 'admin2', 1),
-(111, 110989191219281928, 'Meja', 'lampu 2 TL', 'Centronic', 1, '2024-01-09', 'Sudah disetujui', 'admin', 'admin2', 1),
-(112, 110989191219281929, 'Lampu Penerangan', 'Rak Piring', 'Centronic', 1, '2024-01-09', 'Sudah disetujui', 'admin', 'admin2', 1),
-(113, 110989191219281930, 'Ranjang Tingkat', 'lampu 2 TL', 'Krissbow', 1, '2024-01-09', 'Sudah disetujui', 'admin', 'Almutaqi', 1);
+(116, 112345232112123, 'Aktip Speaker + bluetooth', 'lampu 2 TL', 'Krissbow', 2, '2024-01-09', 'Sudah disetujui', 'admin', 'admin2', 10),
+(117, 112345232112124, 'PC ASUS ROG ', 'Intel Core i7-10350 RAM 32GB VGA NVIDIA RTX 4030TI', 'Untuk dijadikan server lokal', 1, '2024-01-09', 'Sudah disetujui', 'Henda Kurniawan', 'Aldi Taher', 9),
+(118, 112345232112125, 'Genset AE Portabel ', 'Genset AE Portabel ', 'Krissbow', 1, '2024-01-08', 'Sudah disetujui', 'Henda Kurniawan', 'Aldi Taher', 9),
+(119, 112345232112126, 'PC ALL IN ONE', 'Intel Core i5-9400 RAM 8GB DD5 ', 'untuk kebutuhan administrasi kantor', 1, '2024-01-10', 'Sudah disetujui', 'Henda Kurniawan', 'Aldi Taher', 10),
+(120, 112345232112127, 'Mouse Pad', '-', 'Merk Logitech', 1, '2024-01-10', 'Sudah disetujui', 'Henda Kurniawan', 'Aldi Taher', 10);
 
 -- --------------------------------------------------------
 
@@ -65,6 +66,17 @@ CREATE TABLE `karyawan` (
   `status` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_emp`, `nama_emp`, `jabatan`, `divisi`, `status`) VALUES
+(1, 'Muhammad Mabrur Al Mutaqi', 'Staff IT', 'IT', 'Aktif'),
+(2, 'Diki Fadhilah', 'Staff Admin', 'Finance', 'Aktif'),
+(3, 'Henda Kurniawan', 'Staff Purchasing', 'Finance', 'Aktif'),
+(4, 'Aldi Taher', 'Admin Officer', 'Office', 'Aktif'),
+(5, 'Andre Yogi', 'Direktur Operasional', 'Office', 'Aktif');
+
 -- --------------------------------------------------------
 
 --
@@ -76,20 +88,20 @@ CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `level` varchar(200) NOT NULL
+  `level` varchar(200) NOT NULL,
+  `id_emp` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `level`) VALUES
-(1, 'Muhammad Mabrur Al Mutaqi', '$2y$10$n3/GAvaY.GC9Im98tMiPJ.SOGyyxEq118iG5Dk2Ktkic2ez0r4rp6', 'mabrur@gmail.com', 'user'),
-(4, 'admin3', '$2y$10$VMubzUN5RRdfWXenrTgc7uEA0DETX9hh3ht3GmqA4RjRtPKJW1chG', 'adit.hs85@gmail.com', 'admin3'),
-(5, 'diki', '$2y$10$gFV/j7klER8P9lMSTQ7u7ODikYwW472MmhGj4x6z7zxQqAt7bgw6i', 'diki@gmail.com', 'user'),
-(6, 'admin', '$2y$10$qkscpu7F2q6F8O9Vc8ud3OZDu9IvAp4N8eRYrYWVEhUKACfjJ54mG', 'admin@gmail.com', 'admin'),
-(7, 'admin2', '$2y$10$f3bYLOoUrVdWx/2/BH///uiucCZYyGRJwXzXdn4ILSJnDRb6CJugW', 'mabrur@gmail.com', 'admin2'),
-(8, 'Almutaqi', '$2y$10$BMIxPOs8JsfIAwZ2Abi11OXkH6LLrHiahR5.jKiQhTgxdeZqr4twy', 'Almutaqi@gmail.com', 'admin2');
+INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `level`, `id_emp`) VALUES
+(9, 'mabrur', '$2y$10$IFfpvdyJyFuR6GCsvs8hje.Uy8gP41N0B9GijjPs5sgh8xGtnR56u', 'admin@gmail.com', 'user', 1),
+(10, 'diki', '$2y$10$qk3IdIT8A/oiu9lIaEGW/u/CDMgZeTT91wu4F4ww/f1P.EU2.gmpy', 'diki@gmail.com', 'user', 2),
+(11, 'admin', '$2y$10$jgzkM0mK8zHgZlhC7nuTjepzouD91p6z0PaZPp3aymL/BabwifU4e', 'admin@gmail.com', 'admin', 3),
+(12, 'admin2', '$2y$10$lhYFeDPMGWrR9.dVWm.NNe9THUiW.MOStkt/vpHnwrpjvBs9sE3yu', 'admin2@gmail.com', 'admin2', 4),
+(13, 'admin3', '$2y$10$0NepItZYt5bIyKOvPQhRpuGxb..ykUBm2BNLeeQ6iMNPb106CByli', 'admin3@gmail.com', 'admin3', 5);
 
 --
 -- Indexes for dumped tables
@@ -112,7 +124,8 @@ ALTER TABLE `karyawan`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_emp` (`id_emp`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -122,19 +135,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
+  MODIFY `id_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
 
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id_emp` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_emp` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
@@ -145,6 +158,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `barang`
   ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_emp`) REFERENCES `karyawan` (`id_emp`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
